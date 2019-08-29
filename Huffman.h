63620d1675462638b4be4c37db3052f4a10c43f5
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2019-08-29 18:25:49 -0500

    Add files via upload

---
+++ Huffman.h
@@ -0,0 +1,30 @@
+// Dalio, Brian A.
+// dalioba
+// 2019-07-25
+//--------#---------#---------#---------#--------#
+
+typedef struct _HuffmanNode {
+  int frequency;
+  int character;
+  struct _HuffmanNode *left;
+  struct _HuffmanNode *right;
+} HuffmanNode, *HuffmanNodePtr;
+
+typedef struct _HuffmanHeap {
+  HuffmanNodePtr *A;
+  int size;
+  int heapsize;
+} HuffmanHeap, *HuffmanHeapPtr;
+
+//--------#---------#---------#---------#--------#
+
+void addElement( HuffmanHeapPtr h, HuffmanNodePtr newNodePtr );
+HuffmanNodePtr buildHuffmanCode( HuffmanHeapPtr h );
+int dumpHuffmanCode( HuffmanNodePtr n );
+void dumpHuffmanHeap( HuffmanHeapPtr h );
+void dumpHuffmanNode( HuffmanNodePtr n, int indent );
+HuffmanNodePtr extractMin( HuffmanHeapPtr h );
+HuffmanHeapPtr makeHuffmanHeap( int size );
+HuffmanNodePtr makeHuffmanNode( int character, int frequency );
+
+//--------#---------#---------#---------#--------#
